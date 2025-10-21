316b3addfd04defa275b9ab3538200cb196c9961
Deepak <deepakp1976@gmail.com>
2025-10-21 11:50:57 +0530

    Fix: updated layout import and cleaned next.config

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true },
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  },
-  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] }
+    reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    },
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: '**' }
+        ]
+    }
 };
+
 module.exports = nextConfig;
